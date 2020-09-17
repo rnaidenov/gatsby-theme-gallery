e014ea21b84b4f495a5de3a0d1a061ac3a6c0fbb
Radoslav Naydenov <radoslav.naydenov@publicissapient.com>
2020-09-17 16:46:38 +0100

    try sth else

--- gatsby-theme-gallery/src/pages/index.tsx
+++ gatsby-theme-gallery/src/pages/index.tsx
@@ -1,18 +1,11 @@
 import * as React from "react";
 import { Layout } from "theme-ui";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 import Gallery from "../components/Gallery";
-import useSiteMetadata from "../hooks/useSiteMetadata";
 
 const HomePage = () => {
-  const siteMetadata = useSiteMetadata();
-
   return (
     <Layout>
-      <Header />
       <Gallery />
-      {siteMetadata.author && <Footer />}
     </Layout>
   );
 };
